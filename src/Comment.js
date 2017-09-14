d787536a55c7a79ebd30b32b695d07bea6641f66
Andrew <hanoldaa@gmail.com>
2017-09-13 22:49:08 -0400

    level 3 of codeschool react course

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class Comment extends React.Component {
+    
+    constructor(){
+        super();
+
+        this.state = {
+            isAbusive: false
+        }
+    }
+
     render() {
         const commentStyle = {
             paddingBottom: 20
@@ -29,27 +38,48 @@ class Comment extends React.Component {
         }
 
         const linkStyle = {
+            marginRight: 5,
             padding: 5,
             color: 'white',
             fontSize: 12,
             textDecoration: 'none',
             backgroundColor: '#003060',
-            borderRadius: 4
+            borderRadius: 4,
+            borderStyle: 'none'
+        }
+
+        let commentBody;
+        
+        if(!this.state.isAbusive){
+        commentBody = this.props.body;
+        }
+        else{
+        commentBody = <em>Content marked as abusive</em>;
         }
 
         return (
             <div className="comment" style={commentStyle}>
                 <p className="comment-header" style={headerStyle}>{this.props.author}</p>
                 <p className="comment-body" style={bodyStyle}>
-                    {this.props.body}
+                    {commentBody}
                 </p>
                 <div className="comment-footer" style={footerStyle}>
                     <a href="#" className="comment-footer-delete" style={linkStyle}>
                         Delete
                     </a>
+                    <a href="#" onClick={this._toggleAbuse.bind(this)} className="comment-footer-abuse" style={linkStyle}>
+                        Report Abuse
+                    </a>
                 </div>
             </div>
         );
+    }
+      
+    _toggleAbuse(event){
+        event.preventDefault();
+        this.setState({
+        isAbusive: !this.state.isAbusive
+        })
     }
 }
 export default Comment;
--- src/CommentBox.js
+++ src/CommentBox.js
@@ -23,22 +23,63 @@ class CommentBox extends React.Component{
             return (number + ' Comments');
     }
 
+    constructor(){
+        super();
+
+        this.state = {
+            showComments : false
+        };
+    }
+
     render() {
         const comments = this._getComments();
+
         const style = {
             padding: 20,
             color: '#101010',
             fontFamily: 'Ubuntu'
         };
 
+        const headerStyle={
+            display: 'inline-block'
+        }
+
+        const buttonStyle = {
+            marginLeft: 100,
+            padding: 15,
+            fontSize: 20,
+            fontStyle: 'bold',
+            color: 'white',
+            textDecoration: 'none',
+            backgroundColor: '#003060',
+            borderRadius: 10,
+            borderStyle: 'none'
+        }
+
+        let commentNodes;
+        let commentButtonText = 'Show Comments';
+
+        if (this.state.showComments){
+            // Code to show comments
+            commentNodes = <div className="comment-list">{comments}</div>;
+            commentButtonText = 'Hide Comments';
+        }
+
         return (
             <div className="comment-box" style={style}>
-                <h1 className="comment-count">{this._getCommentsTitle(comments.length)}</h1>
-                <div className="comment-list">
-                    {comments}
-                </div>
+                <span style={headerStyle}>
+                    <h1 style={headerStyle} className="comment-count">{this._getCommentsTitle(comments.length)}</h1>
+                    <button onClick={this._handleClick.bind(this)} style={buttonStyle}>{commentButtonText}</button>
+                </span>
+                {commentNodes}
             </div>
         );
+    }
+
+    _handleClick(){
+        this.setState({
+            showComments: !this.state.showComments
+        });
     }
 }
 export default CommentBox;
